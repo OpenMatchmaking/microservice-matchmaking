54c9aabdc4b9921a15213ca9c087b3296a5feb35
Valeryi Savich <relrin78@gmail.com>
2018-03-20 20:45:29 +0300

    Moved application.ex to the root folder

--- matchmaking/lib/application.ex
+++ matchmaking/lib/application.ex
@@ -1,13 +1,14 @@
-defmodule Middleware.Application do
+defmodule Matchmaking.Application do
   @moduledoc false
   use Application
 
   def start(_type, _args) do
     children = [
       {Middleware.Worker, []},
+      {Requeue.Worker, []}
     ]
 
-    opts = [strategy: :one_for_one, name: Middleware.Supervisor]
+    opts = [strategy: :one_for_one, name: Matchmaking.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
