f7e5809d87db53b2892daddf95e60194b924a6c1
Valeryi Savich <relrin78@gmail.com>
2018-03-27 16:52:28 +0300

    Specified rating groups

--- matchmaking/config/config.exs
+++ matchmaking/config/config.exs
@@ -14,17 +14,7 @@ config :matchmaking, Matchmaking.Application,
   generic_queue_workers: {:system, :integer, "GENERIC_QUEUE_WORKERS", 1},
   search_queue_workers: {:system, :integer, "SEARCH_QUEUE_WORKERS", 1},
   requeue_workers: {:system, :integer, "REQUEUE_WORKERS", 1},
-  game_lobby_workers: {:system, :integer, "GAME_LOBBY_WORKERS", 1},
-  # Rating groups
-  groups: [
-    {0,    1499, "bronze"},
-    {1500, 1999, "silver"},
-    {2000, 2499, "gold"},
-    {2500, 2999, "platinum"},
-    {3000, 3499, "diamond"},
-    {3500, 3999, "master"},
-    {4000, 5000, "grandmaster"},
-  ]
+  game_lobby_workers: {:system, :integer, "GAME_LOBBY_WORKERS", 1}
 
 config :matchmaking, Matchmaking.AMQP.Connection,
   username: {:system, "SPOTTER_AMQP_USERNAME", "guest"},
@@ -33,3 +23,14 @@ config :matchmaking, Matchmaking.AMQP.Connection,
   port: {:system, "SPOTTER_AMQP_PORT", 5672},
   virtual_host: {:system, "SPOTTER_AMQP_VHOST", "vhost"},
   connection_timeout: {:system, "SPOTTER_AMQP_TIMEOUT", 60_000}
+
+config :matchmaking, RatingGroups,
+  [
+    {0,    1499, "bronze"},
+    {1500, 1999, "silver"},
+    {2000, 2499, "gold"},
+    {2500, 2999, "platinum"},
+    {3000, 3499, "diamond"},
+    {3500, 3999, "master"},
+    {4000, 5000, "grandmaster"},
+  ]
