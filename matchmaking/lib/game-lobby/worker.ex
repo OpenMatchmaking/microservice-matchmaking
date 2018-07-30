2268d4c35ba6734404ff6139178c285331390321
Valeryi Savich <relrin78@gmail.com>
2018-07-30 14:48:12 +0300

    Added integration with game servers pool microservice

--- matchmaking/lib/game-lobby/worker.ex
+++ matchmaking/lib/game-lobby/worker.ex
@@ -2,8 +2,6 @@ defmodule Matchmaking.Lobby.Worker do
   @moduledoc """
   Worker that sending to each client.
   """
-  # TODO: After sending response for each player delete him from the storage with active users (because it's done)
-  # TODO: Implement and integrate with the game-server microservice
 
   @exchange_request "open-matchmaking.matchmaking.lobby.direct"
   @queue_request "matchmaking.queues.lobbies"
@@ -28,20 +26,80 @@ defmodule Matchmaking.Lobby.Worker do
 
   @exchange_response "open-matchmaking.responses.direct"
 
+  @exchange_game_servers_pool "open-matchmaking.direct"
+  @queue_game_servers_pool "game-servers-pool.server.retrieve"
+
   def configure(channel_name, _opts) do
     consumer = create_consumer(channel_name, @queue_request)
     {:ok, [consumer: consumer]}
   end
 
-  defp get_server_credentials() do
-    # TODO: Replace the fake response onto a real connection data
-    %{
-      "ip" => "127.0.0.1",
-      "port" => 9001,
-      "credentials" => %{
-        "token" => "secret_token"
-      }
-    }
+  defp calculate_required_slots(grouped_players) when is_map(grouped_players) do
+    Enum.sum(Enum.map(grouped_players, fn({_team, players}) -> Enum.count(players) end))
+  end
+
+  defp get_server_credentials(channel_name, game_server_data) when is_map(game_server_data) do
+    request_data = Poison.encode!(game_server_data)
+    send_rpc_request(
+      channel_name, request_data,
+      [exchange_response: @exchange_response,
+       exchange_request: @exchange_game_servers_pool,
+       queue_request: @queue_game_servers_pool]
+    )
+  end
+
+  def prepare_response(player, server_credentials) do
+    Poison.encode!(%{
+      "content" => %{
+        "ip" => server_credentials["ip"],
+        "port" => server_credentials["port"],
+        "team" => team,
+        "credentials" => server_credentials["credentials"]
+      },
+      "event-name" => player["event-name"]
+    })
+  end
+
+  def prepare_error_response(player) do
+    Poison.encode!(%{
+      "error" => %{
+        "type" => "MatchmakingError",
+        "details" => "Occurred an unexpected error during instantiating the game server."
+      },
+      "event-name" => player["event-name"]
+    })
+  end
+
+  def broadcast_connection_data(channel_name, grouped_players, server_credentials) do
+    Enum.map(grouped_players, fn({team, players}) ->
+      Enum.map(players, fn(player) ->
+        queue_name = player["response-queue"]
+        response = prepare_response(player, server_credentials)
+
+        send_response(channel_name, queue_name, response)
+        Matchmaking.Model.ActiveUser.remove_user(player["id"])
+        {:ok}
+      end)
+
+      {team, []}
+      end
+    )
+  end
+
+  def broadcast_error(channel_name, grouped_players) do
+    Enum.map(grouped_players, fn({team, players}) ->
+      Enum.map(players, fn(player) ->
+        queue_name = player["response-queue"]
+        response = prepare_error_response(player)
+
+        send_response(channel_name, queue_name, response)
+        Matchmaking.Model.ActiveUser.remove_user(player["id"])
+        {:ok}
+      end)
+
+      {team, []}
+      end
+    )
   end
 
   defp send_response(channel_name, queue_name, payload) do
@@ -57,34 +115,28 @@ defmodule Matchmaking.Lobby.Worker do
     )
   end
 
-  def broadcast_data(channel_name, grouped_players, server_credentials) do
-    Enum.map(grouped_players, fn({team, players}) ->
-      Enum.map(players, fn(player) ->
-        queue_name = player["response-queue"]
-        response = Poison.encode!(%{
-          "content" => %{
-            "ip" => server_credentials["ip"],
-            "port": server_credentials["port"],
-            "team": team,
-            "credentials": server_credentials["credentials"]
-          },
-          "event-name" => player["event-name"]
-        })
-
-        send_response(channel_name, queue_name, response)
-        Matchmaking.Model.ActiveUser.remove_user(player["id"])
-        {:ok}
-      end)
-
-      {team, []}
-      end
-    )
-  end
-
   def consume(channel_name, tag, _headers, payload) do
-    grouped_players = Poison.decode!(payload)
-    server_credentials = get_server_credentials()
-    spawn fn -> broadcast_data(channel_name, grouped_players, server_credentials) end
+    game_lobby_data = Poison.decode!(payload)
+    grouped_players = game_lobby_data["teams"] || Map.new()
+    server_credentials = get_server_credentials(
+      channel_name,
+      %{
+        "required-slots" => calculate_required_slots(grouped_players),
+        "game-mode" => game_lobby_data["game-mode"]
+      }
+    )
+
+    with true <- is_map(server_credentials),
+         false <- Map.has_key?(server_credentials, "error"),
+         false <- is_nil(server_credentials["content"])
+    do
+      broadcast_connection_data(channel_name, grouped_players, server_credentials)
+    else
+      _ ->
+        Logger.warn "Occurred an error when was requested a game server. Details: #{inspect server_credentials}"
+        broadcast_error(channel_name, grouped_players)
+    end
+
     ack(channel_name, tag)
   end
 end
