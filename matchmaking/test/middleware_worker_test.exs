c674ad9b61206f9891db5c8785cf86f1424356c1
Valeryi Savich <relrin78@gmail.com>
2019-02-13 12:23:52 +0300

    Fixed worker configuration after changes in Elixir

--- matchmaking/test/middleware_worker_test.exs
+++ matchmaking/test/middleware_worker_test.exs
@@ -2,7 +2,7 @@ defmodule MiddlewareWorkerTest do
   use ExUnit.Case
 
   alias Matchmaking.Middleware.Worker, as: MiddlewareWorker
-  import Testing.Helpers.AmqpBlockingClient
+  import Spotter.Testing.AmqpBlockingClient
 
   @rabbitmq_options [
     queue: [
@@ -24,39 +24,41 @@ defmodule MiddlewareWorkerTest do
   ]
 
   setup do
-#    {:ok, middleware_worker} = start_supervised!(%{
-#      id: MiddlewareWorker,
-#      start: {MiddlewareWorker, :start_link, [[channel_name: "Middleware.Worker.Test"]]},
-#      restart: :transient
-#    })
-    {:ok, rabbitmq_client} = start_supervised!(Testing.Helpers.AmqpBlockingClient)
+    middleware_worker = start_supervised!(%{
+      id: MiddlewareWorker,
+      start: {MiddlewareWorker, :start_link, [[channel_name: "Middleware.Worker.Test"]]},
+      restart: :transient
+    })
+    rabbitmq_client = start_supervised!(%{
+      id: Spotter.Testing.AmqpBlockingClient,
+      start: {Spotter.Testing.AmqpBlockingClient, :start_link, [[]]},
+    })
 
     on_exit fn ->
-      # GenServer.stop(middleware_worker)
-      GenServer.stop(rabbitmq_client)
+      GenServer.stop(middleware_worker)
+      #GenServer.stop(Spotter.Testing.AmqpBlockingClient)
     end
 
-    #[process: middleware_worker, #
-    [client: rabbitmq_client]
+     [worker: middleware_worker, client: rabbitmq_client]
   end
 
   test "Middleware pushes the prepared data about the player to the next stage", _context do
-
+    :ok
   end
 
-  test "Middleware returns an error for non existing endpoint", _context do
-
-  end
-
-  test "Middleware returns an error for missing permissions", _context do
-
-  end
-
-  test "Middleware returns an error for the player already in the queue", _context do
-
-  end
-
-  test "Middleware returns an error for an unsuccessful attempt to get information about the player", _context do
-
-  end
+#  test "Middleware returns an error for non existing endpoint", _context do
+#    :ok
+#  end
+#
+#  test "Middleware returns an error for missing permissions", _context do
+#    :ok
+#  end
+#
+#  test "Middleware returns an error for the player already in the queue", _context do
+#    :ok
+#  end
+#
+#  test "Middleware returns an error for an unsuccessful attempt to get information about the player", _context do
+#    :ok
+#  end
 end
